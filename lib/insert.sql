a715b8f6b8603ce072d52e72de91b2585b02f299
ralph1786 <ralph1786@gmail.com>
2018-12-17 12:02:35 -0500

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,49 @@
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 5, "Male", "Brown", "Angry", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Rowdy", 4, "Male", "Gray", "Docile", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Tabitha", 7, "Female", "White", "Happy", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Sergeant Brown", 10, "Male", "Brown", "Angry", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Melissa", 2, "Female", "Black", "Gentle", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Grinch", 8, "Male", "Dark Brown", "Angry", false);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ("Wendy", 5, "Female", "Brown", "Shy", true);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    (NULL, NULL, NULL, NULL, NULL, NULL);
+
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
